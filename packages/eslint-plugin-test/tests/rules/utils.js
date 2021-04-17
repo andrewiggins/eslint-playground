2fd428f260991a341af2a48bb55c4f6c6ab7cec8
Andre Wiggins <andwi@microsoft.com>
2021-04-16 20:07:20 -0700

    Get tests working with full TS typing info

--- packages/eslint-plugin-test/tests/rules/utils.js
+++ packages/eslint-plugin-test/tests/rules/utils.js
@@ -1,21 +1,39 @@
 import { fileURLToPath } from "url";
-import { RuleTester } from "eslint";
+import { dirname, join } from "path";
+import { ESLintUtils } from "@typescript-eslint/experimental-utils";
 
-const parserUrl = await import.meta.resolve("@typescript-eslint/parser");
+const { RuleTester } = ESLintUtils;
 
-export const createRuleTester = () =>
-  new RuleTester({
-    env: {
-      browser: true,
-      es2021: true,
+// Alias mocha names to the names TS ESLint utils expects
+globalThis.afterAll = after;
+globalThis.beforeAll = before;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export const pkgRoot = (...args) => join(__dirname, "..", "..", ...args);
+
+// ESLint RuleTester class requires absolute paths to custom parsers
+// const parserUrl = await import.meta.resolve("@typescript-eslint/parser");
+
+/** @type {import("@typescript-eslint/experimental-utils").TSESLint.RuleTesterConfig} */
+const testConfig = {
+  // env: {
+  //   browser: true,
+  //   es2021: true,
+  // },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    // ESTree parser options from eslint --init
+    ecmaFeatures: {
+      jsx: true,
     },
-    parser: fileURLToPath(parserUrl),
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: "module",
-    },
-    plugins: ["react", "@typescript-eslint"],
-  });
+    ecmaVersion: 2020,
+    sourceType: "module",
+
+    // From TS ESLint types
+    tsconfigRootDir: pkgRoot("tests/fixtures"),
+    project: ["./tsconfig.json"],
+  },
+  // plugins: ["react", "@typescript-eslint"],
+};
+
+export const createRuleTester = () => new RuleTester(testConfig);
